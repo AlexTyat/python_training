076583fa0a2ed12b7a6c205cb1c223718ab2ac78
Alex <alex.tyat@yandex.ru>
2021-03-23 14:48:59 +0200

    add groupe test

--- Test_Add_Group.py
+++ Test_Add_Group.py
@@ -16,7 +16,15 @@ class TestAddGroup(unittest.TestCase):
     def test_add_group(self):
         wd = self.wd
         # open home page
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/group.php")
+    def login(self, wd):
         # login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
@@ -25,12 +33,13 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='Login']").click()
-        #open groups page
+    def open_groups_page(self, wd):
+        # open groups page
         wd.find_element_by_link_text("groups").click()
+    def create_group(self, wd):
         # init groups creation
         wd.find_element_by_name("new").click()
         # fill up group form
-
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("test3333")
@@ -42,11 +51,16 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys("test for test")
         # submit group creation
         wd.find_element_by_name("submit").click()
+    def return_to_groups_page(self, wd):
         # return to groups page
         wd.find_element_by_link_text("groups").click()
+    def logout(self, wd):
         # logout
         wd.find_element_by_link_text("Logout").click()
-    
+
+
+
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
